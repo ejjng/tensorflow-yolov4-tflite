7cf92926e095f164c708201aa493c72ff568d206
ejjng <proej11741@gmail.com>
2022-08-15 23:28:54 +0900

    Refine miou checker

--- detect_parallel.sh
+++ detect_parallel.sh
@@ -1,14 +1,14 @@
-# model_dir=./checkpoints/tflite_models/
-# image_dir=./data/mixed_images/
-# for model_name in "$model_dir"/*
-# do
-# for image_name in "$image_dir"/*
-# do
-#     CUDA_VISIBLE_DEVICES="" python detect.py --name "$(basename -- $model_name)" \
-#         --image_name "$(basename -- $image_name)" &
-# done
-# done
-# wait
+model_dir=./checkpoints/tflite_models/
+image_dir=./data/mixed_images/
+for model_name in "$model_dir"/*
+do
+for image_name in "$image_dir"/*
+do
+    CUDA_VISIBLE_DEVICES="" python detect.py --name "$(basename -- $model_name)" \
+        --image_name "$(basename -- $image_name)" &
+done
+done
+wait
 
 image_dir=./data/mixed_images/
 for model_name in "$model_dir"/*
