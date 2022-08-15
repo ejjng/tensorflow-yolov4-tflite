a8d39111a5cb394ac5c3cc905df59e639902d859
ejjng <proej11741@gmail.com>
2022-08-15 23:07:36 +0900

    Add miou checker

--- detect.py
+++ detect.py
@@ -1,7 +1,4 @@
 import tensorflow as tf
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
@@ -12,6 +9,7 @@ import cv2
 import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+import os
 
 flags.DEFINE_string('framework', 'tflite', '(tf, tflite, trt')
 # flags.DEFINE_string('weights', './checkpoints/yolov4-800-fine-tune-fp16.tflite',
@@ -25,6 +23,7 @@ flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.25, 'score threshold')
 flags.DEFINE_string('name', '', 'type name')
 flags.DEFINE_string('image_name', '', 'type image name')
+flags.DEFINE_boolean('default', False, 'check default')
 
 def main(_argv):
     config = ConfigProto()
@@ -33,8 +32,13 @@ def main(_argv):
     STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
     input_size = FLAGS.size
     image_path = './data/mixed_images/' + FLAGS.image_name
-    weights = './checkpoints/tflite_models/' + FLAGS.name
-    output = './outputs/' + FLAGS.image_name.split('.')[0] + '_' + FLAGS.name.split('.')[0] + '.png'
+    if FLAGS.default:
+        weights = './checkpoints/yolov4-800'
+    else:
+        weights = './checkpoints/tflite_models/' + FLAGS.name
+    output = './outputs/images/' + FLAGS.image_name.split('.')[0] + '_' + FLAGS.name.split('.')[0] + '.png'
+    log_output = './outputs/det_logs/' + FLAGS.image_name.split('.')[0] + '_' + FLAGS.name.split('.')[0] + '.log'
+    log_writer = open(log_output, 'w')
 
     original_image = cv2.imread(image_path)
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
@@ -49,7 +53,7 @@ def main(_argv):
         images_data.append(image_data)
     images_data = np.asarray(images_data).astype(np.float32)
 
-    if FLAGS.framework == 'tflite':
+    if FLAGS.framework == 'tflite' and not FLAGS.default:
         interpreter = tf.lite.Interpreter(model_path=weights)
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
@@ -82,12 +86,15 @@ def main(_argv):
         score_threshold=FLAGS.score
     )
     pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+    for pbb in pred_bbox:
+        log_writer.write(str(pbb.tolist())+'\n')
     image = utils.draw_bbox(original_image, pred_bbox)
     # image = utils.draw_bbox(image_data*255, pred_bbox)
     image = Image.fromarray(image.astype(np.uint8))
     image.show()
     image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
     cv2.imwrite(output, image)
+    log_writer.close()
 
 if __name__ == '__main__':
     try:
--- detect_parallel.sh
+++ detect_parallel.sh
@@ -1,12 +1,21 @@
-model_dir=./checkpoints/tflite_models/
+# model_dir=./checkpoints/tflite_models/
+# image_dir=./data/mixed_images/
+# for model_name in "$model_dir"/*
+# do
+# for image_name in "$image_dir"/*
+# do
+#     CUDA_VISIBLE_DEVICES="" python detect.py --name "$(basename -- $model_name)" \
+#         --image_name "$(basename -- $image_name)" &
+# done
+# done
+# wait
+
 image_dir=./data/mixed_images/
 for model_name in "$model_dir"/*
 do
 for image_name in "$image_dir"/*
 do
-    # echo "$(basename -- $model_name)"
-    # echo "$(basename -- $image_name)"
-    CUDA_VISIBLE_DEVICES="" python detect.py --name "$(basename -- $model_name)" \
+    CUDA_VISIBLE_DEVICES="" python detect.py --default True --name default \
         --image_name "$(basename -- $image_name)" &
 done
 done
